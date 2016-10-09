0041434a14f8890d7146854645ff0f2704e74149
John Leszczynski <jrleszczynski@gmail.com>
2016-10-09 11:10:21 -0400

    add a way to get the lexographic rank of a word amongst all its permutations

--- permutations.py
+++ permutations.py
@@ -10,11 +10,35 @@ def num_permutations(word):
         permutations = permutations * (i + 1) / counts[char]
     return permutations
 
-def unit_test():
-    unit_test_num_permutations()
+def rank(word):
+    """Get the lexicographic order of a word relative to all of its permutations."""
+    counts = Counter(word)
+    ordered = sorted(counts.keys())
+    permutations = num_permutations(word)
+    num_before = 0
+
+    def permutations_without_char(len_word, char):
+        return permutations * counts[char] / len_word
+
+    for i, char in enumerate(word):
+        ordered_chars = iter(ordered)
+        cur = ordered_chars.next()
+        while cur != char:
+            num_before += permutations_without_char(len(word) - i, cur)
+            cur = ordered_chars.next()
+        permutations = permutations_without_char(len(word) - i, char)
+        counts[char] -= 1
+        if not counts[char]:
+            ordered.remove(char)
+    return num_before
+
+
+def __unit_test():
+    __unit_test_num_permutations()
+    __unit_test_rank()
     return "unit_tests pass"
 
-def unit_test_num_permutations():
+def __unit_test_num_permutations():
     for word in '', 'a', 'aa', 'aaa':
         assert num_permutations(word) == 1
     for word in 'ab', 'ba', 'cb', 'xy':
@@ -26,10 +50,15 @@ def unit_test_num_permutations():
 
     from functools import reduce
     for word in 'aababab', 'aba', 'acejadfjdjafj', 'jfkskdfjdkkjjjj':
-        perms = reduce(lambda total, count: total / factorial(count), 
-                       Counter(word).values(), 
+        perms = reduce(lambda total, count: total / factorial(count),
+                       Counter(word).values(),
                        factorial(len(word)))
         assert num_permutations(word) == perms
 
+def __unit_test_rank():
+    for i, word in enumerate(['abc', 'acb', 'bac', 'bca', 'cab', 'cba']):
+        assert rank(word) == i
+
+
 if __name__ == "__main__":
-    print(unit_test())
+    print(__unit_test())
